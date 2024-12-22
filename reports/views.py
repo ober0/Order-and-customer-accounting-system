254e252be5471829727fe4742b63ad883dd1e194
Ruslan <onishruslan@yandex.ru>
2024-12-22 12:54:11 +0300

    +report design

--- reports/views.py
+++ reports/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
 from main.decorators import login_required_custom
+from main.functions import addUserData
 
 
 # Create your views here.
 @login_required_custom
 def reports(request):
-    return render(request, 'reports/reports.html')
+    context = {}
+    context = addUserData(request, context)
+
+    return render(request, 'reports/reports.html', context)
